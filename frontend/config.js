bd42497257859c6acc5e113f78bfe09298b78875
datbi99-prog <datbi99@gmail.com>
2026-07-05 11:47:17 +0700

    Delete frontend/config.js

--- frontend/config.js
+++
@@ -1,2 +0,0 @@
-// DÁN URL WEB APP (kết thúc bằng /exec) VÀO ĐÂY — file cấu hình duy nhất của frontend
-const API_URL = 'https://script.google.com/macros/s/AKfycbwxwCkk8OMI8uDUqSwsDt-f5tg6vP6QTAR_uCyzK395_d8JxkV8BsduUsL1zpCQgyZv/exec';
